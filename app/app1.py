8e83b9c71877ec8db0c0ae7676c4bf29f68704a2
ariel <95013498+matbuha@users.noreply.github.com>
2022-08-06 13:06:33 +0300

    Add files via upload

---
+++ app/app1.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import socket
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return socket.gethostbyname(socket.gethostname())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
